2ada4ed75212cbd6af21fd71535f23c5b9bbb51a
SwagMaster <38248447+DragonBanana@users.noreply.github.com>
2020-07-09 12:40:39 +0200

    Update compress_dataset.sh

--- compress_dataset.sh
+++ compress_dataset.sh
@@ -1,4 +1,4 @@
 mkdir -p Dataset
-gzip -c training.tsv > ./Dataset/train.csv.gz
+gzip -c training.tsv > ./Dataset/new_train.csv.gz
 gzip -c val.tsv > ./Dataset/new_test.csv.gz
 gzip -c test.tsv > ./Dataset/last_test.csv.gz
